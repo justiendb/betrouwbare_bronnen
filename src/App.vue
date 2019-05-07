6dfed50ac3d235efbb839edd6fee9a64ab70fddb
Kwinten De Backer <kwinten.de.backer@be-mobile.com>
2019-05-07 15:44:48 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue';
+import VueFusionCharts from 'vue-fusioncharts';
+import FusionCharts from 'fusioncharts';
+import AngularGauge from 'fusioncharts/fusioncharts.widgets';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
+Vue.use(VueFusionCharts, FusionCharts, AngularGauge, FusionTheme);
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+name: 'app',
+data () {
+return {
+"type": "angulargauge",
+"renderAt": "chart-container",
+"width": "550",
+"height": "350",
+"dataFormat": "json",
+"datasource": {
+"chart": {
+"caption": "Nordstrom's Customer Satisfaction Score for 2017",
+"lowerLimit": "0",
+"upperLimit": "100",
+"showValue": "1",
+"numberSuffix": "%",
+"theme": "fusion",
+"showToolTip": "0"
+},
+// Chart Data
+"colorRange": {
+"color": [{
+"minValue": "0",
+"maxValue": "50",
+"code": "#F2726F"
+}, {
+"minValue": "50",
+"maxValue": "75",
+"code": "#FFC533"
+}, {
+"minValue": "75",
+"maxValue": "100",
+"code": "#62B58F"
+}]
+},
+"dials": {
+"dial": [{
+"value": "81"
+}]
+}
+}
+}
+}
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+<div id="app">
+<div id="chart-container">
+<fusioncharts
+:type="type"
+:width="width"
+:height="height"
+:dataformat="dataformat"
+:datasource="datasource"
+>
+</fusioncharts>
+</div>
+</div>
+</template>
